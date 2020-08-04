1a6ad4831f0aa44a8cbdaa2b481a629e215f0ae6
NeloyNSU <asif.neloy@northsouth.edu>
2020-08-04 10:17:37 +0600

    Assessment added

---
+++ Codes/Lab_3/PRINT_2_STRING.asm
@@ -0,0 +1,42 @@
+
+
+.MODEL SMALL
+.STACK 100H
+
+
+.DATA
+  MSG1 DB "HELLO WORLD$"
+  MSG2 DB "ASSEMBLY LANGUAGE CSE331$"
+  
+.CODE 
+  MAIN PROC 
+    
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    LEA DX, MSG1
+    MOV AH, 9
+    INT 21H
+    
+    MOV AH, 2
+    MOV DL, 0DH
+    INT 21H
+    
+    MOV DL, 0AH
+    INT 21H
+    
+    
+    LEA DX, MSG2
+    MOV AH, 9
+    INT 21H
+    
+    
+    MOV AH, 4CH
+    INT 21H
+    
+ MAIN ENDP
+  END MAIN
+
+
+
+
